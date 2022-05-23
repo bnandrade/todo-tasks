e6c1c8d2fee2310830aab66643ac84f79c0ebea3
Bruno Andrade <bruno_weusa@hotmail.com>
2022-05-23 06:17:19 -0400

    Preparando API para utilizar JWT e criando a rota de Registro de Usuário

---
+++ app/Http/Controllers/JWTController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class JWTController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+
+    public function register(RegisterRequest $request)
+    {
+
+        $user = User::query()->create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Usuário registrado com sucesso',
+            'user' => $user
+        ], 201);
+    }
+}
